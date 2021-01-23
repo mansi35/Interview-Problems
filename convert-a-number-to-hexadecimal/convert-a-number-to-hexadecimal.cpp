4d9a8992c8661845ad17ffeca61b430931ae79c8
Mansi Sharma <53896251+mansi35@users.noreply.github.com>
2021-01-23 09:20:58 +0530

    Time: 0 ms (100.00%), Space: 6 MB (87.17%) - LeetHub

---
+++ convert-a-number-to-hexadecimal/convert-a-number-to-hexadecimal.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string toHex(int num) {
+        if (num == 0)
+            return "0";
+        unsigned int num1 = num;
+        int base = 16;
+        string hex = "";
+        int rem;
+        
+        while (num1 > 0) {
+            rem = num1 % base;
+            if (rem < 10)
+                hex += '0' + rem;
+            else
+                hex += 'a' + (rem % 10);
+            num1 = num1 / base;
+        }
+        reverse(hex.begin(), hex.end());
+        return hex;
+    }
+};
